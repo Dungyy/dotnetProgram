12717c9b53f3c0872a8d1e889bcdf978e974c4b2
Erick <erickmunoz13@gmail.com>
2023-11-08 23:56:04 -0600

    Basic Intro into variables for c#

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Terminal title
 Console.Title = "Dungy bot";
+// Terminal Text color
+Console.ForegroundColor = ConsoleColor.Green;
 
-Console.ForegroundColor = ConsoleColor.DarkMagenta;
+double num1;
+double num2;
 
 Console.WriteLine("Hello, whats your name?");
+string userName = Console.ReadLine();
 
-Console.ReadLine();
+Console.WriteLine("Hello " + userName + " It's nice to meet you!");
+Console.WriteLine(userName + " Give me an number from 0-100");
 
-Console.WriteLine("My name is dungyBot-9000.\nAI sent from the future to destroy mankind.");
-Console.WriteLine("Whats your favorite color? ");
+num1 = Convert.ToDouble(Console.ReadLine());
 
-Console.ReadKey();
+Console.WriteLine(num1 + " ok cool, and another number?");
 
-Console.WriteLine("cool,! Mine is destruction");
+num2 = Convert.ToDouble(Console.ReadLine());
 
+double result = num1 * num2;
+
+Console.WriteLine("So does " + num1 + "x" + num2 + " = " + result + "?");
+
+// wait to close terminal
 Console.ReadKey();
