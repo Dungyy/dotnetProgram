1a2a3a7cf9d29be138d4adf1e8a8077361a94a3c
Erick <erickmunoz13@gmail.com>
2023-11-08 23:24:35 -0600

    Basic Intro

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.Title = "Dungy bot";
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+Console.WriteLine("Hello, whats your name?");
+
+Console.ReadLine();
+
+Console.WriteLine("My name is dungyBot-9000.\nAI sent from the future to destroy mankind.");
+Console.WriteLine("Whats your favorite color? ");
+
+Console.ReadKey();
+
+Console.WriteLine("cool,! Mine is destruction");
+
+Console.ReadKey();
